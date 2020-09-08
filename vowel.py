f48fbeacdd519f70d906532db573c8a6e8eaba7d
Renugadevi-S <70885262+Renugadevi-S@users.noreply.github.com>
2020-09-08 15:31:57 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,7 @@
+#program to find out the given element is vowel or constant
+
+def alpha(ele):
+    if ele=="a" or "e" or "i" or "o" or "u":
+        return "vowel"
+    return "consonant"
+print(alpha("ea"))
